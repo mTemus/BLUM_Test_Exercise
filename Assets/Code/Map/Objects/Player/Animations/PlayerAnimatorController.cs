3d68dd72b4fdd07c3a048c0f90989e664cfe722f
mTemus <temus@wir.pl>
2022-07-23 16:48:03 +0200

    Script cleanup

--- Assets/Code/Map/Objects/Player/Animations/PlayerAnimatorController.cs
+++ Assets/Code/Map/Objects/Player/Animations/PlayerAnimatorController.cs
@@ -1,19 +1,10 @@
 using UnityEngine;
 using Zenject;
 
-public class PlayerAnimatorController : NestedComponent
+public class PlayerAnimatorController : Object2DAnimatorController
 {
-    public string VelocityXParameterName;
-    public string VelocityYParameterName;
-    public string IsGroundedParameterName;
-
     private IEventsManager m_eventsManager;
-    private Animator m_animator;
-    private ObjectPhysics2DState m_physics2DState;
-
-    private float m_velocityX;
     private float m_velocityY;
-    private bool m_isGrounded;
 
     [Inject]
     private void Construct(IEventsManager eventsManager)
@@ -21,25 +12,11 @@ public class PlayerAnimatorController : NestedComponent
         m_eventsManager = eventsManager;
     }
 
-    private void Start()
+    protected override void OnVelocityChanged(SimpleValueBase value)
     {
-        m_animator = GetComponent<Animator>();
-        m_physics2DState = GetComponentInRoot<ObjectPhysics2DState>();
-
-        m_physics2DState.IsGrounded.AddChangedListener(OnGroundedChanged);
-        m_physics2DState.Velocity.AddChangedListener(OnVelocityChanged);
-    }
-
-    private void OnVelocityChanged(SimpleValueBase value)
-    {
+        base.OnVelocityChanged(value);
         var velocity = value.GetValueAs<Vector2>();
 
-        if (m_velocityX != velocity.x)
-        {
-            m_animator.SetFloat(VelocityXParameterName, Mathf.Abs(velocity.x));
-            m_velocityX = velocity.x;
-        }
-        
         if (m_velocityY != velocity.y)
         {
             m_animator.SetFloat(VelocityYParameterName, velocity.y);
@@ -47,17 +24,7 @@ public class PlayerAnimatorController : NestedComponent
         }
     }
 
-    private void OnGroundedChanged(SimpleValueBase value)
-    {
-        var isGrounded = value.GetValueAs<bool>();
-
-        if (m_isGrounded == isGrounded)
-            return;
-
-        m_animator.SetBool(IsGroundedParameterName, isGrounded);
-        m_isGrounded = isGrounded;
-    }
-
+    // Called by animation event
     public void OnJumpStart()
     {
         if (m_physics2DState.IsGrounded.Value)
@@ -66,6 +33,7 @@ public class PlayerAnimatorController : NestedComponent
         m_eventsManager.CallEvent(PlayerObjectEvents.OnJumpStart, transform.parent.position);
     }
 
+    // Called by animation event
     public void OnJumpEnd()
     {
         if (!m_physics2DState.IsGrounded.Value)
---
+++ Assets/Code/Map/Objects/Universal/Animations2D/Object2DAnimatorController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+public class Object2DAnimatorController : NestedComponent
+{
+    public interface AnimationEndListener
+    {
+        void OnAnimationEnded();
+    }
+
+    public const string VelocityXParameterName = "VelocityX";
+    public const string VelocityYParameterName = "VelocityY";
+    public const string IsGroundedParameterName = "IsGrounded";
+
+    [SerializeField] 
+    private readonly bool UpdateVelocity = true;
+
+    [SerializeField] 
+    private readonly bool UpdateIsGrounded = true;
+
+    private AnimationEndListener m_listener = null;
+    protected Animator m_animator;
+    protected ObjectPhysics2DState m_physics2DState;
+
+    private float m_velocityX;
+    private bool m_isGrounded;
+    private bool m_animStarted = true;
+    private bool m_animFinished = true;
+
+    private int m_currAnimNameHash = 0;
+
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_physics2DState = GetComponentInRoot<ObjectPhysics2DState>();
+
+        if (UpdateVelocity)
+            m_physics2DState.Velocity.AddChangedListener(OnVelocityChanged);
+
+        if (UpdateIsGrounded)
+            m_physics2DState.IsGrounded.AddChangedListener(OnGroundedChanged);
+
+        enabled = false;
+    }
+
+    protected virtual void OnVelocityChanged(SimpleValueBase value)
+    {
+        var velocity = value.GetValueAs<Vector2>();
+
+        if (m_velocityX != velocity.x)
+        {
+            m_animator.SetFloat(VelocityXParameterName, Mathf.Abs(velocity.x));
+            m_velocityX = velocity.x;
+        }
+
+    }
+
+    private void OnGroundedChanged(SimpleValueBase value)
+    {
+        var isGrounded = value.GetValueAs<bool>();
+
+        if (m_isGrounded == isGrounded)
+            return;
+
+        m_animator.SetBool(IsGroundedParameterName, isGrounded);
+        m_isGrounded = isGrounded;
+    }
+
+    public void PlayAnimation(string animFullPath, AnimationEndListener listener, float speed = 1f)
+    {
+        m_animator.Play(animFullPath, 0, 0.0f);
+        PerformPlayAnimCommon(animFullPath, listener, speed);
+    }
+
+    private void PerformPlayAnimCommon(string animFullPath, AnimationEndListener listener, float speed)
+    {
+        m_listener = listener;
+
+        m_currAnimNameHash = Animator.StringToHash(animFullPath);
+        enabled = true;
+        m_animStarted = false;
+        m_animFinished = false;
+
+        m_animator.speed *= speed;
+    }
+
+    private void Update()
+    {
+        var animInfo = m_animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animInfo.fullPathHash == m_currAnimNameHash)
+        {
+            m_animStarted = true;
+
+            if (animInfo.normalizedTime >= 1.0f)
+                m_animFinished = true;
+        }
+        else
+        {
+            if (m_animStarted)
+                m_animFinished = true;
+        }
+
+        if (!m_animFinished) 
+            return;
+
+        m_animator.speed = 1.0f;
+
+        enabled = false;
+
+        m_listener?.OnAnimationEnded();
+    }
+}
