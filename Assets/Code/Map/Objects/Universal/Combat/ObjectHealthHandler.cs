1439613ea7e7e42ce6db477d5f2705f9dd65db92
mTemus <temus@wir.pl>
2022-07-28 10:55:39 +0200

    Add cooldown for getting damage by object

--- Assets/Code/Map/Objects/Universal/Combat/ObjectHealthHandler.cs
+++ Assets/Code/Map/Objects/Universal/Combat/ObjectHealthHandler.cs
@@ -1,6 +1,12 @@
-﻿public class ObjectHealthHandler : NestedComponent
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObjectHealthHandler : NestedComponent
 {
+    public float HurtCooldownInSeconds;
     private ObjectHealthState m_healthState;
+
+    private bool m_canBeHurt = true;
 
     private void Awake()
     {
@@ -11,15 +17,25 @@
 
     public void GetHurt(int damage)
     {
+        if (!m_canBeHurt)
+            return;
+
+        m_canBeHurt = false;
+
         var health = m_healthState.Health.Value;
         health -= damage;
 
         m_healthState.Health.Value = health;
 
         if (health <= 0)
+        {
             Die();
+        }
         else
+        {
             GetHurtInternal();
+            StartCoroutine(CountHurtCooldown());
+        }
     }
 
     protected virtual void GetHurtInternal() { }
@@ -33,5 +49,11 @@
     private void OnDeath(string eventName, object data)
     {
         Destroy(transform.parent.gameObject);
+    }
+
+    private IEnumerator CountHurtCooldown()
+    {
+        yield return new WaitForSeconds(HurtCooldownInSeconds);
+        m_canBeHurt = true;
     }
 }
