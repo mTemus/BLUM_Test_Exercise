0b24915657371344c5cd78c3027a03d917efbe31
mTemus <temus@wir.pl>
2022-07-30 17:13:44 +0200

    Update interaction symbol, add automatic handling when gamepad gets connected/disconnected

--- Assets/Code/Map/Objects/Player/Effects/InteractionInputEffectHandler.cs
+++ Assets/Code/Map/Objects/Player/Effects/InteractionInputEffectHandler.cs
@@ -1,23 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
 public class InteractionInputEffectHandler : MonoBehaviour
 {
+    public enum InputControllerType
+    {
+        Keyboard, Gamepad
+    }
+
     [SerializeField]
     private float m_yOffset;
 
     private PlayerAccessInterface m_player;
+    private InputControllerType m_controller;
+    private Animator m_animator;
 
     [Inject]
     private void Construct(PlayerAccessInterface player)
     {
         m_player = player;
+        InputSystem.onDeviceChange += OnDeviceChanged;
         enabled = false;
+        m_animator = GetComponent<Animator>();
+        m_controller = Gamepad.current != null ? InputControllerType.Gamepad : InputControllerType.Keyboard;
     }
 
-    private void Awake()
+    private void OnDeviceChanged(InputDevice device, InputDeviceChange changeType)
     {
-        enabled = false;
+
+        if (m_controller == InputControllerType.Keyboard)
+        {
+            if (device is Gamepad && changeType == InputDeviceChange.Added)
+            {
+                Debug.Log("Keyboard disabled");
+                m_controller = InputControllerType.Gamepad;
+            }
+        }
+        else
+        {
+            if (device is Gamepad && changeType == InputDeviceChange.Removed)
+            {
+                Debug.Log("Gamepad disabled");
+                m_controller = InputControllerType.Keyboard;
+            }
+        }
+
+        PlaySymbol();
     }
 
     private void Update()
@@ -30,11 +59,17 @@ public class InteractionInputEffectHandler : MonoBehaviour
     {
         gameObject.SetActive(true);
         enabled = true;
+        PlaySymbol();
     }
 
     public void Hide()
     {
         enabled = false;
         gameObject.SetActive(false);
+    }
+
+    private void PlaySymbol()
+    {
+        m_animator.Play(m_controller == InputControllerType.Keyboard ? "Keyboard" : "Gamepad");
     }
 }
