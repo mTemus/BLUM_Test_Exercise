1a52eec1bb379a6a587398b3a002f01265cd87e2
mTemus <temus@wir.pl>
2022-08-07 11:04:46 +0200

    Update attacking mechanism for objects

---
+++ Assets/Code/Map/Objects/Universal/Combat/ObjectAttackHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public abstract class ObjectAttackHandler : NestedComponent
+{
+    [SerializeField]
+    protected Transform m_attackPoint;
+
+    [SerializeField]
+    protected LayerMask m_enemiesLayerMask;
+
+    protected ObjectAttackState m_attackState;
+
+    protected void Awake()
+    {
+        m_attackState = GetComponentInRoot<ObjectAttackState>();
+        m_attackState.IsAttacking.AddChangedListener(OnAttack, false);
+    }
+
+    private void OnAttack(SimpleValueBase value)
+    {
+        var isAttacking = value.GetValueAs<bool>();
+
+        if (!isAttacking)
+            return;
+
+        OnAttackInternal();
+    }
+
+    protected abstract void OnAttackInternal();
+}
--- Assets/Code/Map/Objects/Universal/Combat/ObjectMeleeAttackHandler.cs
+++ Assets/Code/Map/Objects/Universal/Combat/ObjectMeleeAttackHandler.cs
@@ -1,36 +1,17 @@
 ﻿using UnityEngine;
 
-public class ObjectMeleeAttackHandler : NestedComponent
+public class ObjectMeleeAttackHandler : ObjectAttackHandler
 {
-    [SerializeField]
-    private Transform m_attackPoint;
-    
     [SerializeField]
     private Vector2 m_attackPointSize;
 
-    [SerializeField] 
-    private LayerMask m_enemiesLayerMask;
-
-    private ObjectAttackState m_attackState;
-
-    void Awake()
+    protected override void OnAttackInternal()
     {
-        m_attackState = GetComponentInRoot<ObjectAttackState>();
-        m_attackState.IsAttacking.AddChangedListener(OnAttack, false);
-    }
-
-    private void OnAttack(SimpleValueBase value)
-    {
-        var isAttacking = value.GetValueAs<bool>();
-
-        if (!isAttacking)
-            return;
-
         var enemiesHit = Physics2D.OverlapBoxAll(m_attackPoint.position, m_attackPointSize, 0, m_enemiesLayerMask);
 
         foreach (var enemy in enemiesHit)
             enemy.GetComponentInChildren<ObjectHealthHandler>().GetHurt(m_attackState.BaseDamage.Value);
-        
+
         m_attackState.IsAttacking.Value = false;
     }
 
@@ -42,5 +23,4 @@ public class ObjectMeleeAttackHandler : NestedComponent
     }
 
 #endif
-
 }
