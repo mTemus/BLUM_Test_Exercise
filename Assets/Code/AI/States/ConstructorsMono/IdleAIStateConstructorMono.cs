2e1ddc9139408d4909a942bd6563823c69fb3eb9
mTemus <temus@wir.pl>
2022-08-07 11:06:52 +0200

    Add AI idle state

---
+++ Assets/Code/AI/States/ConstructorsMono/IdleAIStateConstructorMono.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class IdleAIStateConstructorMono : AIStateConstructorMono
+{
+    public Vector3 IdlePosition;
+    public bool ReturnToPosition = true;
+
+    public override AIState Construct(ObjectGenericAIController aiController)
+    {
+        var package = new IdleAIState.IdleAIStatePackage
+        {
+            Controller = aiController,
+            IdlePosition = IdlePosition,
+            ReturnToPosition = ReturnToPosition
+        };
+
+        return new IdleAIState(package);
+    }
+
+    public override void Clear()
+    {
+        Destroy(GetComponent<IdleAIStateConstructorMono>());
+    }
+}
---
+++ Assets/Code/AI/States/IdleAIState.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class IdleAIState : AIState
+{
+    public class IdleAIStatePackage : AIStatePackage
+    {
+        public Vector3 IdlePosition;
+        public bool ReturnToPosition;
+    }
+
+    public IdleAIState(AIStatePackage package) : base(package)
+    {
+        AIStateType = AIStateType.Idle;
+
+        var concretePackage = package as IdleAIStatePackage;
+        m_idlePosition = concretePackage.IdlePosition;
+        m_movement = package.Controller.GetComponentInRoot<ObjectMovement2DController>();
+    }
+
+    private ObjectMovement2DController m_movement;
+
+    private Vector3 m_idlePosition;
+    private bool m_isOnPosition;
+    private bool m_returnToPosition;
+
+    public override void Update(ObjectGenericAIController controller)
+    {
+        if (!m_returnToPosition)
+            return;
+        
+        if (m_isOnPosition)
+            return;
+
+        if (m_movement.MoveOnXTo())
+            m_isOnPosition = true;
+    }
+
+    public override void OnStateSet()
+    {
+        if (!m_returnToPosition)
+            return;
+
+        m_movement.PrepareToMoveOnXTo(m_idlePosition.x);
+    }
+
+    public override void OnStateChanged()
+    {
+        if (!m_returnToPosition)
+            return;
+
+        m_isOnPosition = false;
+        m_movement.PrepareToMoveOnXTo(m_idlePosition.x);
+    }
+}
